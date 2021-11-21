92041418166d9e2784798dd9034c31f651d5548e
martin <mocon@DESKTOP-1GQAV5L>
2021-11-21 19:31:10 +0100

    sd

--- js/index.js
+++ js/index.js
@@ -2,6 +2,6 @@ const projectCards = document.querySelectorAll('.project-card-content');
 projectCards.forEach(projectCard=>{
   // let bodyTextLength = projectCard.childNodes[3].offsetHeight;
   console.log('lines = ', projectCard, projectCard.offsetHeight)
-  let cardsize = projectCard.offsetHeight - 51;
+  let cardsize = projectCard.offsetHeight - 55;
   projectCard.style.transform = `translateY(${cardsize}px)`;
 })
