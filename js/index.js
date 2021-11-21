5da7ac35ef5281bfd88ffee0cf7ce239d2f3b19c
martin <mocon@DESKTOP-1GQAV5L>
2021-11-21 19:20:32 +0100

    Add Projects

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-const menuIn = ()=>{
-  const menuDiv = document.querySelector(".menu");
-  const menuItems = document.querySelectorAll('.menu-items');
-  
-}
-
-const menuOut = ()=>{}
+const projectCards = document.querySelectorAll('.project-card-content');
+projectCards.forEach(projectCard=>{
+  // let bodyTextLength = projectCard.childNodes[3].offsetHeight;
+  console.log('lines = ', projectCard, projectCard.offsetHeight)
+  let cardsize = projectCard.offsetHeight - 51;
+  projectCard.style.transform = `translateY(${cardsize}px)`;
+})
