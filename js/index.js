4d447f71d3b013489332e7aa9d402207e2f8b8bb
martin <mocon@DESKTOP-1GQAV5L>
2021-11-22 18:24:20 +0100

    DES

--- js/index.js
+++ js/index.js
@@ -1,13 +1,32 @@
+//Offset the content by its size
 const projectCards = document.querySelectorAll('.project-card-content');
 projectCards.forEach(projectCard=>{
-  // let bodyTextLength = projectCard.childNodes[3].offsetHeight;
-  console.log('lines = ', projectCard, projectCard.offsetHeight)
   let cardsize = projectCard.offsetHeight - 55;
   projectCard.style.transform = `translateY(${cardsize}px)`;
 })
 
 
-function reportWindowSize() {
-  console.log(window.innerWidth,window.innerHeight)
-}
-window.onresize = reportWindowSize;
+// ROOM
+//Update gallery items
+const galleryRoomMain =  document.getElementById('gallery-room-main');
+const galleryRoomImgs = document.querySelectorAll('.gallery-room-img');
+
+galleryRoomImgs.forEach((img,i)=>{
+  img.addEventListener('click', ()=>{
+    let newUrl = `../img/projects/room/${1+i}.jpg`;
+    galleryRoomMain.style.backgroundImage = `url(${newUrl})`;
+  })
+})
+
+//Arrow hide function
+const backBtnRoom =  document.getElementById('back-btn-room');
+const projectsContainerRoom =  document.getElementById('projects-container-room');
+backBtnRoom.addEventListener('click', ()=>{
+  projectsContainerRoom.classList.add('project-info-card-hidden');
+});
+
+// More info btn functionality
+const moreInfoBtn = document.getElementById('more-info-btn-room');
+moreInfoBtn.addEventListener('click', ()=>{
+  projectsContainerRoom.classList.remove('project-info-card-hidden');
+})
